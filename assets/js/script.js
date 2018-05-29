a31ac5ee9b211d5aaac47a04e815faf958b654c8
rukmana4985 <rukmana4985@gmail.com>
2018-05-30 01:40:24 +1000

    Page 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
     $(".button-collapse").sideNav();
+    $('.stars li').on('click', function() {
+        var el = $(this);
+        el.addClass('active').siblings().removeClass('active');
+        $('#rating').val( el.attr('title') ); // save value
+    });
 })
